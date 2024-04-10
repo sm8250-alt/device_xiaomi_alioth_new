1bc30ee33335172da44a3556949fc7064cbe36e9
levi3609 <99073465+levi3609@users.noreply.github.com>
2024-04-10 07:24:33 +0600

    Alioth: croot to back project root folder

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,4 @@
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
 cd kernel/xiaomi/sm8250 && git submodule init && git submodule update
+croot

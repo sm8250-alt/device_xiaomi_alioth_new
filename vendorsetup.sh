279f250bbfe2844821f82aa45e81c916b2f08f29
levi3609 <99073465+levi3609@users.noreply.github.com>
2024-04-10 07:10:59 +0600

    Alioth: submodule init for kernel

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 base64 -d device/xiaomi/alioth/configs/camera/secret > device/xiaomi/alioth/configs/camera/st_license.lic
+cd kernel/xiaomi/sm8250 && git submodule init && git submodule update
